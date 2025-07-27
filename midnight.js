07f965ada33e236c20db5fdbab18ee3bd155035f
KemonoBAT4 <kemono.bat.4@gmail.com>
2025-07-27 04:50:21 +0200

    [midnight] various changes to the desktop application

--- midnight.js
+++ midnight.js
@@ -1,7 +1,10 @@
 
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
-function createWindow() {
+const iconPath = path.join(__dirname, "core", "assets", "icons", "midnight.svg");
+
+function midnightWindow() {
     const win = new BrowserWindow({
         show: false,
         autoHideMenuBar: true,
@@ -12,12 +15,13 @@ function createWindow() {
         webPreferences: {
             nodeIntegration: true
         }
+
     });
 
     win.maximize();
     win.show();
 
-    win.loadFile('core/views/test.html');
+    win.loadFile('core/views/midnightDesktop.html');
 }
 
 
@@ -28,9 +32,9 @@ app.on('window-all-closed', () => {
 });
 
 app.whenReady().then(() => {
-    createWindow();
+    midnightWindow();
 
     if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
+        midnightWindow();
     }
 });
